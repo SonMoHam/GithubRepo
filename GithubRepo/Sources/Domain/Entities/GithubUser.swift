11cbbc880d455dd2f5c72e4215fe81f92ead12d0
Sonmoham <dnddl505@gmail.com>
2022-11-14 15:33:48 +0900

    [Add] 도메인 엔티티 GithubUser 추가

---
+++ GithubRepo/Sources/Domain/Entities/GithubUser.swift
@@ -0,0 +1,13 @@
+//
+//  GithubUser.swift
+//  GithubRepo
+//
+//  Created by Son Daehong on 2022/11/14.
+//
+
+import Foundation
+
+struct GithubUser {
+    let name: String
+    let followers: Int
+}
