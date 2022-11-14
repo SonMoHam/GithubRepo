702c24a89021b42f5989bc2ceef9d25012a5b185
Sonmoham <dnddl505@gmail.com>
2022-11-14 18:40:11 +0900

    [Feat] 네트워크 - NetworkService 추가

---
+++ GithubRepo/Sources/Network/NetworkService.swift
@@ -0,0 +1,47 @@
+//
+//  NetworkService.swift
+//  GithubRepo
+//
+//  Created by Son Daehong on 2022/11/14.
+//
+
+import Foundation
+import Alamofire
+
+public enum NetworkError: Error {
+    case urlGeneration
+    case responseDecoding
+}
+
+public protocol NetworkService {
+    typealias CompletionHandler = (Result<Decodable, Error>) -> Void
+    func request<R, E>(
+        with endpoint: E,
+        completion: @escaping CompletionHandler
+    ) where R : Decodable, R == E.Response, E : Requestable
+}
+
+public final class AFNetworkService {
+    
+}
+
+extension AFNetworkService: NetworkService {
+    public func request<R, E>(
+        with endpoint: E,
+        completion: @escaping CompletionHandler
+    ) where R : Decodable, R == E.Response, E : Requestable {
+        guard let urlRequest = endpoint.asURLRequest() else {
+            completion(.failure(NetworkError.urlGeneration))
+            return
+        }
+        AF.request(urlRequest)
+            .responseDecodable(of: R.self) { response in
+                switch response.result {
+                case .success(let decodable):
+                    completion(.success(decodable))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+}
