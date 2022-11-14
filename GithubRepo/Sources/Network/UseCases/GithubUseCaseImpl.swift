1238cfd4a5961b3d76ee792bb315784fa64f645e
Sonmoham <dnddl505@gmail.com>
2022-11-14 18:48:19 +0900

    [Add] 네트워크 - UseCases 추가

---
+++ GithubRepo/Sources/Network/UseCases/GithubUseCaseImpl.swift
@@ -0,0 +1,14 @@
+//
+//  GithubUseCase.swift
+//  GithubRepo
+//
+//  Created by Son Daehong on 2022/11/14.
+//
+
+import Foundation
+import RxSwift
+
+public final class DefaultGithubUseCase { }
+
+// TODO: 더미 엔티티 반환
+public final class StubGithubUseCase { }
