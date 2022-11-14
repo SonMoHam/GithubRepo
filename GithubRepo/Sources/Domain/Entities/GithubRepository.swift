ebcf7e1e7355176a20ec42349fef2a8ff261464b
Sonmoham <dnddl505@gmail.com>
2022-11-14 15:35:05 +0900

    [Add] 도메인 엔티티 GithubRepository 추가

---
+++ GithubRepo/Sources/Domain/Entities/GithubRepository.swift
@@ -0,0 +1,14 @@
+//
+//  GithubRepository.swift
+//  GithubRepo
+//
+//  Created by Son Daehong on 2022/11/14.
+//
+
+import Foundation
+
+struct GithubRepository {
+    let name: String
+    let description: String?
+    let language: String?
+}
