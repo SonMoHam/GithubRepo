4064f8df22ac0d2e4742b89021e1b61e4b4fd8c4
Sonmoham <dnddl505@gmail.com>
2022-11-15 18:12:25 +0900

    [Feat] RepositoryViewReactor 추가

---
+++ GithubRepo/Sources/Presentation/Scenes/Repository/RepositoryViewReactor.swift
@@ -0,0 +1,50 @@
+//
+//  RepositoryViewReactor.swift
+//  GithubRepo
+//
+//  Created by Son Daehong on 2022/11/15.
+//
+
+import ReactorKit
+import RxSwift
+
+final class RepositoryViewReactor: Reactor {
+    
+    enum Action {
+        case refresh
+    }
+    
+    enum Mutation {
+        case setRepositories([GithubRepository])
+    }
+    
+    struct State {
+        var repositories: [GithubRepository]
+    }
+    
+    let userName = "sonmoham"
+    let initialState: State
+    let useCase: GithubUseCase
+    
+    init(useCase: GithubUseCase) {
+        self.initialState = State(repositories: [])
+        self.useCase = useCase
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .refresh:
+            return self.useCase.fetchRepositories(of: userName)
+                .map { .setRepositories($0) }
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case .setRepositories(let repos):
+            state.repositories = repos
+            return state
+        }
+    }
+}
