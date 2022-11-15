57fd9ad2cc42752e45c7004a20c4fe49d8aaf5b7
Sonmoham <dnddl505@gmail.com>
2022-11-15 14:47:05 +0900

    [Add] add empty UserInfo, Repository VC

---
+++ GithubRepo/Sources/Presentation/Scenes/Repository/RepositoryViewController.swift
@@ -0,0 +1,8 @@
+//
+//  RepositoryViewController.swift
+//  GithubRepo
+//
+//  Created by Son Daehong on 2022/11/15.
+//
+
+import Foundation
---
+++ GithubRepo/Sources/Presentation/Scenes/UserInfo/UserInfoViewController.swift
@@ -0,0 +1,8 @@
+//
+//  UserInfoViewController.swift
+//  GithubRepo
+//
+//  Created by Son Daehong on 2022/11/15.
+//
+
+import Foundation
