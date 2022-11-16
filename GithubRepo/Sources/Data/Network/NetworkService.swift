78c5e22a2d67a629e3e2d9329bcf0b1e97a5a309
Sonmoham <dnddl505@gmail.com>
2022-11-16 15:56:46 +0900

    [Feat] NetworkService - decodable decode 메소드 추가

--- GithubRepo/Sources/Data/Network/NetworkService.swift
+++ GithubRepo/Sources/Data/Network/NetworkService.swift
@@ -44,4 +44,14 @@ extension AFNetworkService: NetworkService {
                 }
             }
     }
+    
+    private func decode<R: Decodable>(data: Data?) -> Result<R, Error> {
+        do {
+            guard let data = data else { return .failure(NetworkError.responseDecoding)}
+            let decoded: R = try JSONDecoder().decode(R.self, from: data)
+            return .success(decoded)
+        } catch {
+            return .failure(NetworkError.responseDecoding)
+        }
+    }
 }
