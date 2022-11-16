a946bffe67737ac48329a2add880e89977e2fd7d
Sonmoham <dnddl505@gmail.com>
2022-11-16 14:14:04 +0900

    [Feat] RepositoryVC - repository tableView 추가, RepositoryCell 적용

--- GithubRepo/Sources/Presentation/Scenes/Repository/RepositoryViewController.swift
+++ GithubRepo/Sources/Presentation/Scenes/Repository/RepositoryViewController.swift
@@ -14,10 +14,16 @@ import Then
 
 final class RepositoryViewController: UIViewController, View {
     
+    // MARK: Constants
+    
+    private struct Metric {
+        static let verticalMargin: CGFloat = 50
+        static let horizontalMargin: CGFloat = 30
+    }
+    
     // MARK: Properties
     
     var disposeBag = DisposeBag()
-
     
     lazy var tableViewLabel = UILabel().then {
         let userName = self.reactor?.userName ?? "user"
@@ -25,9 +31,11 @@ final class RepositoryViewController: UIViewController, View {
         $0.textAlignment = .left
     }
     
-    let label = UILabel().then {
-        $0.numberOfLines = 0
-        $0.text = "RepositoryVC"
+    let tableView = UITableView().then {
+        $0.register(RepositoryCell.self, forCellReuseIdentifier: RepositoryCell.reuseIdentifier)
+        $0.layer.borderWidth = 2
+        $0.layer.borderColor = UIColor.systemGray6.cgColor
+        $0.layer.cornerRadius = 5
     }
     
     // MARK: Initializing
@@ -47,7 +55,7 @@ final class RepositoryViewController: UIViewController, View {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(tableViewLabel)
-        view.addSubview(label)
+        view.addSubview(tableView)
         setupConstraints()
     }
     
@@ -55,12 +63,15 @@ final class RepositoryViewController: UIViewController, View {
     
     func setupConstraints() {
         tableViewLabel.snp.makeConstraints { make in
-            make.top.left.right.equalToSuperview().inset(30)
+            make.top.equalToSuperview().inset(Metric.verticalMargin)
+            make.left.right.equalToSuperview().inset(Metric.horizontalMargin)
             make.height.equalTo(20)
         }
         
-        label.snp.makeConstraints { make in
-            make.center.equalToSuperview()
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(tableViewLabel.snp.bottom).offset(10)
+            make.left.right.equalToSuperview().inset(Metric.horizontalMargin)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(Metric.verticalMargin)
         }
     }
     
@@ -82,12 +93,13 @@ private extension RepositoryViewController {
     
     func bindState(_ reactor: RepositoryViewReactor) {
         reactor.state.asObservable()
-            .map {
-                $0.repositories
-                    .map { "\($0.name), \($0.language ?? "")"}
-                    .joined(separator: "\n")
-            }
-            .bind(to: label.rx.text)
-            .disposed(by: disposeBag)
+            .map { $0.repositories }
+            .bind(to: tableView.rx.items) { (tableView, index, element) in
+                guard let cell = tableView.dequeueReusableCell(
+                    withIdentifier: RepositoryCell.reuseIdentifier) as? RepositoryCell
+                else { return UITableViewCell() }
+                cell.configure(element)
+                return cell
+            }.disposed(by: disposeBag)
     }
 }
