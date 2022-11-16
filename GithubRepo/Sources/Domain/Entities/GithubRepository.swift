1f0ef38d0b46fa98d5f070d5f8ab40f9ac0541be
Sonmoham <dnddl505@gmail.com>
2022-11-16 16:21:42 +0900

    [Feat] 도메인 GithubRepository Codable로 변경, 프로퍼티 추가, 코딩키 추가

--- GithubRepo/Sources/Domain/Entities/GithubRepository.swift
+++ GithubRepo/Sources/Domain/Entities/GithubRepository.swift
@@ -7,8 +7,14 @@
 
 import Foundation
 
-public struct GithubRepository: Decodable {
+public struct GithubRepository: Codable {
     let name: String
     let description: String?
     let language: String?
+    let svnURL: String?
+    
+    enum CodingKeys: String, CodingKey {
+      case name, description, language
+      case svnURL = "svn_url"
+    }
 }
