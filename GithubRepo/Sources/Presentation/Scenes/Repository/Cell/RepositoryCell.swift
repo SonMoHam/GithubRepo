ab499e0b66bd1704204695e51bd6c4041b4e4c21
Sonmoham <dnddl505@gmail.com>
2022-11-16 10:57:14 +0900

    [Add] empty RepositoryCell 추가

---
+++ GithubRepo/Sources/Presentation/Scenes/Repository/Cell/RepositoryCell.swift
@@ -0,0 +1,17 @@
+//
+//  RepositoryCell.swift
+//  GithubRepo
+//
+//  Created by Son Daehong on 2022/11/16.
+//
+
+import UIKit
+
+import Then
+import ReactorKit
+import RxSwift
+import SnapKit
+
+final class RepositoryCell: UITableViewCell {
+    
+}
